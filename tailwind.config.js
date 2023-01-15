997a017afdd5f6dfaae413412175a8197e4b6d80
Jakub Cichon <jakub@uconn.edu>
2023-01-15 03:42:11 -0500

    add Contact section w/ contact form (not done yet)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,32 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('tailwindcss'), require('autoprefixer'), require('daisyui')],
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+		require('@tailwindcss/forms'),
+		require('daisyui'),
+	],
 	daisyui: {
 		themes: [
+
+		],
+	},
+	daisyui: {
+		themes: [
+			{
+				customNight: {
+					primary: '#3ABFF8',
+					secondary: '#828DF8',
+					accent: '#F471B5',
+					neutral: '#1D283A',
+					'base-100': '#191f2e',
+					info: '#0CA6E9',
+					success: '#2BD4BD',
+					warning: '#F4C152',
+					error: '#FB6F84',
+				},
+			},
 			'light',
 			'dark',
 			'cupcake',
